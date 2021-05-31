9a1ee056b2a12ab3dafd92b908c8afa1d07a7613
Timo <tklaasee@gmail.com>
2021-06-01 00:25:04 +0200

    Implement FuelBar 1/2

--- src/layout/MainLayout/TopBar.js
+++ src/layout/MainLayout/TopBar.js
@@ -1,8 +1,7 @@
-import {AppBar, IconButton, makeStyles, Toolbar, Typography, Box} from "@material-ui/core";
-import {AccountBalanceWallet} from "@material-ui/icons";
+import {AppBar, makeStyles, Toolbar, Typography, Box, Grid, Button} from "@material-ui/core";
 import clsx from "clsx";
-import {Link as RouterLink} from "react-router-dom";
-import Logo from "../../components/Logo";
+import React, {useEffect, useState} from "react";
+const Big = require('big-js');
 
 const topBarStyles = makeStyles((theme) => ({
     root: {
@@ -15,7 +14,9 @@ const topBarStyles = makeStyles((theme) => ({
         marginRight: theme.spacing(2)
     },
     topBarText: {
-        color: theme.palette.twinkle.main
+        color: theme.palette.twinkle.main,
+        mixBlendMode: "difference",
+        padding: '0.3em'
     },
     priceContainer: {
         display: "flex",
@@ -26,9 +27,17 @@ const topBarStyles = makeStyles((theme) => ({
 const TopBar = ({className, ...rest}) => {
     const classes = topBarStyles();
 
-    const connectWallet = () => {
+    const [initFuel, setInitFuel] = useState(false);
+    const [fuel, setFuel] = useState(2452103);
+    const [initialFuel, setInitialFuel] = useState(6307200);
+    const [fuelPercentage, setFuelPercentage] = useState(new Big(0));
 
-    }
+    useEffect(() => {
+        if (!initFuel) {
+            setFuelPercentage(new Big(fuel).div(initialFuel).times(100));
+            setInitFuel(true);
+        }
+    }, [initFuel]);
 
     return (
         <AppBar
@@ -37,17 +46,34 @@ const TopBar = ({className, ...rest}) => {
             {...rest}
         >
             <Toolbar className={classes.toolBar}>
-                <RouterLink to={"/"}>
+                <Grid container justify={"space-between"} style={{
+                    background: `linear-gradient(to right, #F5BC00 ${initFuel ? fuelPercentage : 100}%, #0A0A0A ${initFuel ? fuelPercentage : 0}%)`,
+                    borderRadius: 20
+                }}>
                     {/*<Logo className={classes.logo}/>*/}
-                </RouterLink>
-                <Typography variant={"subtitle1"} className={classes.topBarText}>
-                    VOLUME
-                </Typography>
-                <Box flexGrow={1} className={classes.priceContainer}>
-                    <Typography variant={"h4"} className={classes.topBarText}>
-                        Price: $1.27
-                    </Typography>
-                </Box>
+
+                    <Grid item>
+                        <Typography variant={"h4"} className={classes.topBarText}>
+                            VOLUME
+                        </Typography>
+                    </Grid>
+
+                    <Grid item>
+                        <Typography variant="h4" className={classes.topBarText}>
+                            {
+                                `${Math.round(new Big(fuel).div(initialFuel).times(100))}%`
+                            }
+                        </Typography>
+                    </Grid>
+
+                    <Grid item>
+                        <Box flexGrow={1} className={classes.priceContainer}>
+                            <Typography variant={"h4"} className={classes.topBarText}>
+                                Price: $1.27
+                            </Typography>
+                        </Box>
+                    </Grid>
+                </Grid>
             </Toolbar>
         </AppBar>
     );
