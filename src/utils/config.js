f06d8f53f383a6f80151ed5c9660b79036c30d55
Timo <tklaasee@gmail.com>
2021-06-09 04:08:09 +0200

    New contract address

--- src/utils/config.js
+++ src/utils/config.js
@@ -1,5 +1,5 @@
 module.exports = {
-    volumeAddress: '0xcBeaBAEd67aEc4f065db266a400C30A6404e632C', // This is the MainNet address for Volume
+    volumeAddress: '0xce029d651416Dc23Fa190b7108928F854cCc694d', // This is the MainNet address for Volume
     testVolumeAddress: '0x2D6cbda46E80310f46A56e955636A92c3d49B891', // This is currently the testnet address for TestVolume
     localUrl: 'ws://localhost:8545',
     testNetUrl: 'https://data-seed-prebsc-1-s1.binance.org:8545',
