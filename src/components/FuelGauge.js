b4949cf19e441605f3d081672c4de4effd1d5ac0
Timo <tklaasee@gmail.com>
2021-06-19 21:17:38 +0200

    Prepped for new dash

---
+++ src/components/FuelGauge.js
@@ -0,0 +1,17 @@
+import {
+    Container,
+} from "@material-ui/core";
+import React from "react";
+
+const FuelGauge = () => {
+
+    return (
+        <Container
+            maxWidth={false}
+        >
+        {/*    Have fun    */}
+        </Container>
+    )
+}
+
+export default FuelGauge;
---
+++ src/components/Leaderboard.js
@@ -0,0 +1,17 @@
+import {
+    Container,
+} from "@material-ui/core";
+import React from "react";
+
+const Leaderboard = () => {
+
+    return (
+        <Container
+            maxWidth={false}
+        >
+            {/*    Have fun    */}
+        </Container>
+    )
+}
+
+export default Leaderboard;
