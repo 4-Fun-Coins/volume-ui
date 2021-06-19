ce6af254ee3cb0d25b050860ec235b99c2112053
Timo <tklaasee@gmail.com>
2021-06-19 22:46:19 +0200

    Removed top bar

--- src/layout/MainLayout/index.js
+++ src/layout/MainLayout/index.js
@@ -1,5 +1,4 @@
 import {makeStyles} from "@material-ui/core";
-import TopBar from "./TopBar";
 import PropTypes from "prop-types";
 import {UseWalletProvider} from "use-wallet";
 
@@ -15,7 +14,6 @@ const mainLayoutStyle = makeStyles((theme) => ({
         display: "flex",
         flex: "1 1 auto",
         overflow: "hidden",
-        paddingTop: 64
     },
     contentContainer: {
         display: "flex",
@@ -40,7 +38,6 @@ const MainLayout = ({children}) => {
                 pollBalanceInterval={2000}
                 pollBlockNumberInterval={5000}
             >
-                <TopBar/>
                 <div className={classes.wrapper}>
                     <div className={classes.contentContainer}>
                         <div className={classes.content}>
