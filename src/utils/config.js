f6840ed94b96b3fef4cb743b26e39246c4bcf422
Timo <tklaasee@gmail.com>
2021-06-20 21:56:42 +0200

    Changed address/config to kovan

--- src/utils/config.js
+++ src/utils/config.js
@@ -1,7 +1,7 @@
 module.exports = {
-    volumeAddress: '0xce029d651416Dc23Fa190b7108928F854cCc694d', // This is the MainNet address for Volume
-    testVolumeAddress: '0x2D6cbda46E80310f46A56e955636A92c3d49B891', // This is currently the testnet address for TestVolume
+    volumeTestAddress: '0x8c590BF25Dc6c6EA45353B8011B6712a3B0bd6Ec', // This is the MainNet address for Volume
+    testVolumeAddress: '0x8c590BF25Dc6c6EA45353B8011B6712a3B0bd6Ec', // This is currently the testnet address for TestVolume
     localUrl: 'ws://localhost:8545',
-    testNetUrl: 'https://data-seed-prebsc-1-s1.binance.org:8545',
+    testNetUrl: 'https://kovan.poa.network/',
     mainNetUrl: 'https://bsc-dataseed1.binance.org'
 }
