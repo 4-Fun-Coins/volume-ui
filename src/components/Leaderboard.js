4bb1cd89f81f3350b431a355f64535bf24babc42
Timo <tklaasee@gmail.com>
2021-06-21 11:53:53 +0200

    Implemented leaderboard + user wallet tracking

--- src/components/Leaderboard.js
+++ src/components/Leaderboard.js
@@ -4,6 +4,9 @@ import {
 } from "@material-ui/core";
 import React, {useEffect, useState} from "react";
 import LeaderboardEntry from "./LeaderboardEntry";
+import {getSortedLeaderboard} from "../utils/volume-core";
+import {useWallet} from "use-wallet";
+import {utils} from "web3";
 
 const leaderboardStyles = makeStyles((theme) => ({
     lbWrapper: {
@@ -23,30 +26,62 @@ const leaderboardStyles = makeStyles((theme) => ({
 
 const Leaderboard = () => {
     const classes = leaderboardStyles();
+    const wallet = useWallet();
 
     const [lbInit, setLbInit] = useState(false);
     const [leaderboard, setLeaderboard] = useState([]);
 
+    const [userLbPosition, setUserLbPosition] = useState(undefined);
+
     useEffect(() => {
         if (!lbInit) {
             // Load leaderboard here
+            getSortedLeaderboard().then((sortedLeaderboard) => {
+                const tempArr = [];
+                if (sortedLeaderboard.length < 10) {
+                    for (let i = 0; i < 10 - sortedLeaderboard.length; i++) {
+                        tempArr.push({
+                            nickname: "no_player",
+                            fuelAdded: '0'
+                        });
+                    }
+                    setLeaderboard(sortedLeaderboard.concat(tempArr));
+                } else {
+                    // Only take first 10 and look for connected user address
+                    let foundUser = false;
+                    for (let i = 0; i < 10; i++) {
+                        tempArr.push(sortedLeaderboard[i]);
+                        if (wallet.status === 'connected' && sortedLeaderboard[i].user === wallet.address) {
+                            foundUser = true;
+                        }
+                    }
 
-            const tempData = [];
-            // Dummy data
-            for (let i = 0; i < 10; i++) {
-                tempData.push({
-                    thisUser: false,
-                    name: 'Test' + i,
-                    fuel: i
-                });
-            }
-
-            tempData[5].thisUser = true;
-
-            setLeaderboard(tempData);
-            setLbInit(true);
+                    // if we still did not find the user address, it might be somewhere else in the array
+                    if (!foundUser) {
+                        // Make sure it is in the array
+                        if (wallet.status === 'connected' && sortedLeaderboard.find(entry => {return entry.user === wallet.address})) {
+                            for (let i = 10; i < sortedLeaderboard.length; i++) {
+                                if (sortedLeaderboard[i].user === wallet.address) {
+                                    setUserLbPosition({
+                                        nickname: sortedLeaderboard[i].nickname,
+                                        user: sortedLeaderboard[i].user,
+                                        fuelAdded: sortedLeaderboard[i].fuelAdded,
+                                        number: i + 1
+                                    });
+                                }
+                            }
+                        }
+                    }
+                }
+                setLbInit(true);
+            });
         }
     }, [lbInit]);
+
+    useEffect(() => {
+        if (wallet.status === 'connected')
+            setLbInit(false);
+    }, [wallet.status]);
 
     return (
             <Grid container item direction={"column"} justify={"space-evenly"} className={classes.lbWrapper} spacing={1}>
@@ -79,9 +114,32 @@ const Leaderboard = () => {
                 {lbInit &&
                     leaderboard.map((entry, i) => {
                         return (
-                            <LeaderboardEntry key={i} number={i + 1} name={entry.name} fuelAdded={entry.fuel} thisUser={entry.thisUser}/>
+                            <LeaderboardEntry
+                                key={i}
+                                number={i + 1}
+                                name={entry.nickname ? entry.nickname : entry.user}
+                                fuelAdded={utils.fromWei(entry.fuelAdded)}
+                                thisUser={wallet.status === 'connected' ? wallet.address === entry.user : false}
+                            />
                         )
                     })
+                }
+
+                {userLbPosition &&
+                    <>
+                        <LeaderboardEntry
+                            key={"emptyUser"}
+                            number={".."}
+                            name={"..."}
+                            fuelAdded={"..."}
+                        />
+                        <LeaderboardEntry
+                            key={"thisUser"}
+                            number={userLbPosition.number}
+                            name={userLbPosition.nickname !== "" ? userLbPosition.nickname : userLbPosition.user}
+                            fuelAdded={userLbPosition.fuelAdded}
+                        />
+                    </>
                 }
             </Grid>
     )
