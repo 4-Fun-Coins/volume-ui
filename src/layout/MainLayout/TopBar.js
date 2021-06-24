2505ee52ae4350496e15bbc5f55307e4b7d019d6
Timo <tklaasee@gmail.com>
2021-06-24 17:19:02 +0200

    Implemented page switches - disabled

--- src/layout/MainLayout/TopBar.js
+++ src/layout/MainLayout/TopBar.js
@@ -1,10 +1,24 @@
-import {AppBar, Button, Divider, Grid, makeStyles, Toolbar} from "@material-ui/core";
+import {
+    AppBar,
+    Button, CssBaseline,
+    Divider, Drawer,
+    Grid, Hidden, IconButton,
+    List,
+    ListItem,
+    ListItemIcon,
+    ListItemText,
+    makeStyles,
+    Toolbar
+} from "@material-ui/core";
 import clsx from "clsx";
-import React, {useEffect} from "react";
+import React, {useEffect, useState} from "react";
 import Typography from "@material-ui/core/Typography";
 import {useWallet} from "use-wallet";
 import {useHistory} from 'react-router-dom';
 import {ROUTES_NAMES} from "../../constants";
+import MenuIcon from "@material-ui/icons/Menu";
+
+const drawerWidth = 240;
 
 const topBarStyles = makeStyles((theme) => ({
     root: {
@@ -23,6 +37,16 @@ const topBarStyles = makeStyles((theme) => ({
     },
     button: {
         padding: '1em'
+    },
+    emptyBar: theme.mixins.toolbar,
+    drawerPaper: {
+        width: drawerWidth,
+        opacity: 0.9,
+        backgroundColor: "#1d0134"
+    },
+    drawerListItem: {
+        color: theme.palette.twinkle.main,
+        paddingLeft: '2em'
     }
 }));
 
@@ -31,10 +55,17 @@ const TopBar = ({className, changeNetwork, network, ...rest}) => {
     const wallet = useWallet();
     const history = useHistory();
 
-    useEffect(() => {
-        console.log(wallet.status);
-        console.log(wallet.error);
-    }, [wallet.status]);
+    const toHome = () => {
+        history.push(ROUTES_NAMES.HOME);
+    }
+
+    const toJourney = () => {
+        history.push(ROUTES_NAMES.JOURNEY);
+    }
+
+    const toRefuel = () => {
+        history.push(ROUTES_NAMES.REFUEL);
+    }
 
     const newNetwork = (netId) => {
         if (network !== netId) {
@@ -43,85 +74,173 @@ const TopBar = ({className, changeNetwork, network, ...rest}) => {
         }
     }
 
-    return (
-        <AppBar
-            color={"primary"}
-            className={clsx(classes.root, className)}
-            {...rest}
-        >
-            <Toolbar className={classes.toolBar}>
-                <Grid container justify={"flex-start"} xs={3}>
-                    <Button onClick={() => {
-                        history.push(ROUTES_NAMES.HOME);
+    // ========================== Mobile optimization
+    const [mobileOpen, setMobileOpen] = useState(false);
+    const handleDrawerToggle = () => {
+        setMobileOpen(!mobileOpen);
+    }
+
+    const drawer = (
+        <div>
+            <div className={classes.emptyBar}/>
+            <Divider />
+            <List>
+                <ListItem button key={'Home'} onClick={toHome}>
+                    <ListItemText primary={'Home'} className={classes.drawerListItem}/>
+                </ListItem>
+                <ListItem button key={'Journey'} onClick={toJourney}>
+                    <ListItemText primary={"The Journey"} className={classes.drawerListItem}/>
+                </ListItem>
+                <ListItem button key={'Refuel'} onClick={toRefuel}>
+                    <ListItemText primary={"Refuel"} className={classes.drawerListItem}/>
+                </ListItem>
+            </List>
+            <Divider />
+            <Grid container>
+                <Grid container item xs={12} justify={"center"}>
+                    <Button variant={"text"} className={classes.button} onClick={() => {
+                        if (wallet.status !== 'connected')
+                            wallet.connect();
                     }}>
-                        <Typography variant={"h2"} className={classes.volText}>
-                            Volume
+                        <Typography variant={"h4"} className={classes.volText}>
+                            {
+                                wallet.status === 'connected'
+                                    ? `${wallet.account.slice(0, 6)}...${wallet.account.slice(wallet.account.length-4, wallet.account.length)}`
+                                    : 'Connect'
+                            }
                         </Typography>
                     </Button>
                 </Grid>
 
-                <Grid container xs={7} alignItems={"center"} justify={"flex-start"} spacing={4}>
-                    {/*Journey*/}
-                    <Grid className={classes.button} container item xs={6}>
-                        <Button onClick={() => {
-                            // history.push(ROUTES_NAMES.JOURNEY);
-                        }}>
-                            <Typography variant={"h4"} className={classes.volText}>
-                                The Journey
-                            </Typography>
-                        </Button>
-                    </Grid>
-
-                    {/*Details*/}
-                    <Grid container item xs={6}>
-                        <Button className={classes.button} onClick={() => {
-                            // history.push(ROUTES_NAMES.INFO);
-                        }}>
-                            <Typography variant={"h4"} className={classes.volText}>
-                                More Info
-                            </Typography>
-                        </Button>
-                    </Grid>
+                <Grid container item xs={6} justify={"center"}>
+                    <Button variant={"text"} className={classes.button} onClick={() => {
+                        newNetwork(56);
+                    }}>
+                        <Typography variant={"h4"} className={network === 56 ? classes.boldText : classes.volText}>
+                            BSC
+                        </Typography>
+                    </Button>
                 </Grid>
 
-                <Grid container item justify={"flex-end"} xs={12}>
-                    <Grid container item xs={6} justify={"flex-end"}>
-                        <Button variant={"text"} className={classes.button} onClick={() => {
-                            newNetwork(56);
-                        }}>
-                            <Typography variant={"h4"} className={network === 56 ? classes.boldText : classes.volText}>
-                                BSC
-                            </Typography>
-                        </Button>
-
-                        <Divider orientation={"vertical"} variant={"fullWidth"} style={{width: '1px',backgroundColor: "grey"}}/>
-
-                        <Button variant={"text"} className={classes.button} onClick={() => {
-                            newNetwork(42);
-                        }}>
-                            <Typography variant={"h4"} className={network === 42 ? classes.boldText : classes.volText}>
-                                Kovan
-                            </Typography>
-                        </Button>
-                    </Grid>
-
-                    <Grid container item xs={2} justify={"flex-end"}>
-                        <Button variant={"text"} className={classes.button} onClick={() => {
-                            if (wallet.status !== 'connected')
-                                wallet.connect();
-                        }}>
-                            <Typography variant={"h4"} className={classes.volText}>
-                                {
-                                    wallet.status === 'connected'
-                                        ? `${wallet.account.slice(0, 6)}...${wallet.account.slice(wallet.account.length-4, wallet.account.length)}`
-                                        : 'Connect'
-                                }
-                            </Typography>
-                        </Button>
-                    </Grid>
+                <Grid container item xs={6} justify={"center"}>
+                    <Button variant={"text"} className={classes.button} onClick={() => {
+                        newNetwork(42);
+                    }}>
+                        <Typography variant={"h4"} className={network === 42 ? classes.boldText : classes.volText}>
+                            Kovan
+                        </Typography>
+                    </Button>
                 </Grid>
-            </Toolbar>
-        </AppBar>
+            </Grid>
+        </div>
+    );
+    // ==========================
+
+    return (
+        <div>
+            <AppBar
+                position={"fixed"}
+                color={"primary"}
+                className={clsx(classes.root, className)}
+                {...rest}
+            >
+                <Toolbar className={classes.toolBar}>
+                    <Hidden mdUp>
+                        <IconButton className={classes.button} onClick={() => {
+                            setMobileOpen(true);
+                        }}>
+                            <MenuIcon className={classes.volText}/>
+                        </IconButton>
+                    </Hidden>
+
+                    <Hidden smDown>
+                        <Grid container justify={"flex-start"} xs={3}>
+                            <Button onClick={toHome}>
+                                <Typography variant={"h2"} className={classes.volText}>
+                                    Volume
+                                </Typography>
+                            </Button>
+                        </Grid>
+
+                        <Grid container xs={7} alignItems={"center"} justify={"flex-start"} spacing={4}>
+                            {/*Journey*/}
+                            <Grid container item xs={6}>
+                                <Button className={classes.button} onClick={toJourney}>
+                                    <Typography variant={"h4"} className={classes.volText}>
+                                        The Journey
+                                    </Typography>
+                                </Button>
+                            </Grid>
+
+                            {/*Details*/}
+                            <Grid container item xs={6}>
+                                <Button className={classes.button} onClick={toRefuel}>
+                                    <Typography variant={"h4"} className={classes.volText}>
+                                        Refuel
+                                    </Typography>
+                                </Button>
+                            </Grid>
+                        </Grid>
+
+                        <Grid container item justify={"flex-end"} xs={12}>
+                            <Grid container item xs={6} justify={"flex-end"}>
+                                <Button variant={"text"} className={classes.button} onClick={() => {
+                                    newNetwork(56);
+                                }}>
+                                    <Typography variant={"h4"} className={network === 56 ? classes.boldText : classes.volText}>
+                                        BSC
+                                    </Typography>
+                                </Button>
+
+                                <Divider orientation={"vertical"} variant={"fullWidth"} style={{width: '1px',backgroundColor: "grey"}}/>
+
+                                <Button variant={"text"} className={classes.button} onClick={() => {
+                                    newNetwork(42);
+                                }}>
+                                    <Typography variant={"h4"} className={network === 42 ? classes.boldText : classes.volText}>
+                                        Kovan
+                                    </Typography>
+                                </Button>
+                            </Grid>
+
+                            <Grid container item xs={2} justify={"flex-end"}>
+                                <Button variant={"text"} className={classes.button} onClick={() => {
+                                    if (wallet.status !== 'connected')
+                                        wallet.connect();
+                                }}>
+                                    <Typography variant={"h4"} className={classes.volText}>
+                                        {
+                                            wallet.status === 'connected'
+                                                ? `${wallet.account.slice(0, 6)}...${wallet.account.slice(wallet.account.length-4, wallet.account.length)}`
+                                                : 'Connect'
+                                        }
+                                    </Typography>
+                                </Button>
+                            </Grid>
+                        </Grid>
+                    </Hidden>
+
+                </Toolbar>
+            </AppBar>
+
+            <Hidden mdUp>
+                <Drawer
+                    variant="temporary"
+                    anchor={"left"}
+                    open={mobileOpen}
+                    onClose={handleDrawerToggle}
+                    classes={{
+                        paper: classes.drawerPaper,
+                    }}
+                    ModalProps={{
+                        keepMounted: true, // Better open performance on mobile.
+                    }}
+                >
+                    {drawer}
+                </Drawer>
+            </Hidden>
+        </div>
+
     );
 }
 
