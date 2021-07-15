c681bac5f2c428f5c0c613f3407468c77bfce859
Timo <tklaasee@gmail.com>
2021-07-15 11:17:24 +0200

    Updated addresses

--- src/utils/config.js
+++ src/utils/config.js
@@ -1,5 +1,6 @@
 module.exports = {
-    volumeAddress: '0xF4084FFbec3C84Cc63B880521793E10855BBE499', // Change this to main/test depending on your use case
+    volumeAddress: '0xD934AfDCe00240B9878521083989453c9A63B4d7', // Change this to main/test depending on your use case
+    volumeJackpotAddress: '0xABdb4c4a4849ad26DB7FC9F7D352174B83F77623',
     rpcUrl: 'https://kovan.poa.network/', // 'https://bsc-dataseed1.binance.org'
     chainId: 42
 }
