1f263332d18d9251637b2d8d5a416bf9875a8916
Rachid Boudjelida <rachidboudjelida@gmail.com>
2021-08-08 22:24:11 +0100

    make topbar logo clickable

--- src/components/LogoWithText.js
+++ src/components/LogoWithText.js
@@ -1,12 +1,15 @@
 import Box from '@material-ui/core/Box';
 import { makeStyles , Typography} from "@material-ui/core";
+import {useHistory} from 'react-router-dom';
+import { ROUTES_NAMES } from '../constants';
 
 const styles = makeStyles((theme) => ({
     root : {
         display: 'flex',
         justifyContent: 'center',
         alignItems: 'center',
-        padding: 2
+        padding: 2,
+        cursor: 'Pointer',
     },
     volText: {
         color: theme.palette.twinkle.main,
@@ -16,10 +19,11 @@ const styles = makeStyles((theme) => ({
 );
 
 const LogoWithText = (props) => {
+    const history = useHistory();
     const classes = styles();
 
     return (
-        <Box className={classes.root}>
+        <Box className={classes.root} onClick={() => {history.push(ROUTES_NAMES.HOME)}}>
             <img src={'/logo_small.png'} width={'48'} height={'48'} alt={'logo'}/>
             <Typography variant="h6" className={classes.volText}>
                             Volume
