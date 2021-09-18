9f60ec8c007ef946a0903905fffe37cf942f92f7
Timo <tklaasee@gmail.com>
2021-09-18 18:25:13 +0200

    Added testnet

--- src/utils/config.js
+++ src/utils/config.js
@@ -2,5 +2,11 @@ module.exports = {
     volumeAddress: '0xD934AfDCe00240B9878521083989453c9A63B4d7', // Change this to main/test depending on your use case
     volumeJackpotAddress: '0xABdb4c4a4849ad26DB7FC9F7D352174B83F77623',
     rpcUrl: 'https://kovan.poa.network/', // 'https://bsc-dataseed1.binance.org'
-    chainId: 42
+    chainId: 42,
+
+    // Test Net configs
+    kovanChainId: 42,
+    bscTestnetId: 97,
+    bsdTestnetExplorer: 'https://testnet.bscscan.com/',
+    volumeFaucetAddress: '0x13308Bf72C0D2AE6221507CaF6263bd66ddB9410',
 }
