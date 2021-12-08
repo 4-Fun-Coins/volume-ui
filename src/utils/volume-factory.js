819a5e2adf9a70c8f84d2949fbb7dcbc30b7257e
Timo <tklaasee@gmail.com>
2021-12-08 15:34:08 +0200

    Added getCategories

--- src/utils/volume-factory.js
+++ src/utils/volume-factory.js
@@ -1,5 +1,9 @@
 const {ipfsGateway} = require('./config.js');
-const Big = require('big.js');
+const {VolumeFactoryAbi} = require("./abis/volume-factory-abi");
+const {volumeFactory, rpcUrl} = require("./config");
+
+const Web3 = require('web3');
+let web3 = new Web3(rpcUrl);
 
 const getJSONFromIPFS = async (hash) => {
     return new Promise((res, rej) => {
@@ -20,10 +24,17 @@ const getJSONFromIPFS = async (hash) => {
     });
 }
 
-const getCollections = async () => {
+// const getAddCreatorData = (newCreator) => {
+//     const factory = new web3.eth.Contract(VolumeFactoryAbi, volumeFactory);
+//     return factory.methods.addCreator(newCreator).encodeABI();
+// }
 
+const getCollections = async () => {
+    const ino = new web3.eth.Contract(VolumeFactoryAbi, volumeFactory);
+    return ino.methods.getAllCategories().call();
 }
 
 module.exports = {
-    getJSONFromIPFS
+    getJSONFromIPFS,
+    getCollections,
 }
